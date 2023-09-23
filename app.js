a1c39a74d5cb9851fd5552a7bd72591e1b4cd84d
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-23 16:01:00 +0530

    add mathematical functions and operate functions

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 
+const numbers = document.querySelector('.numbers');
+
+const numberBtns = numbers.querySelectorAll('button');
+
+const operators = document.querySelector('.operators');
+
+const operatorBtns = operators.querySelectorAll('button');
 
 
+let result = 0;
+let currentNumber = 0;
+let operator;
+
+numberBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+
+    currentNumber = Number(e.target.textContent);
+
+  })
+})
+
+
+operatorBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+
+    operator = e.target.textContent;
+  })
+})
 //functions
 function add(a, b) {
   return a + b;
@@ -17,3 +43,22 @@ function multiply(a, b) {
 function division(a, b) {
   return a / b;
 }
+
+function operate(result, currentNumber, operator) {
+
+  if (operator == "+") {
+    add(result, currentNumber);
+  }
+
+  if (operator == "-") {
+    subtract(result, currentNumber);
+  }
+
+  if (operator == "x") {
+    multiply(result, currentNumber);
+  }
+  if (operator == "/") {
+    division(result, currentNumber);
+  }
+}
+
